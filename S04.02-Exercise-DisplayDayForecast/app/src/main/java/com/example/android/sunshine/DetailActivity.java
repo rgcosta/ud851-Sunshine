e336cf4c54e4e67a675c6187bb84344963753753
Romulo Costa <rgcostabox@gmail.com>
2018-04-10 21:01:43 -0300

    Complete all tasks from the S4.2 ToDo list

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,17 +1,29 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    private TextView mdisplayTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        // DONE (2) Display the weather forecast that was passed from MainActivity
+        this.mdisplayTextView = (TextView) findViewById(R.id.tv_display_weather);
+
+        Intent intent = getIntent();
+
+        if(intent.hasExtra(Intent.EXTRA_TEXT)){
+            String extraText = intent.getStringExtra(Intent.EXTRA_TEXT);
+            mdisplayTextView.setText(extraText);
+        }
     }
 }
